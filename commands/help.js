0785198f2b9e7ef16e9862353f16cb357fcb0fed
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-07-10 15:32:41 +0545

    Updated help.js

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,48 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const packageJson = require('../package.json');
+const fs = require('fs');
+const path = require('path');
+const packageJson = require('../package.json'); 
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Displays help information.'),
+        .setDescription('Displays help information and bot statistics.'),
     async execute(interaction) {
         const client = interaction.client;
+        const commandFiles = fs.readdirSync(path.resolve(__dirname, '../commands')).filter(file => file.endsWith('.js'));
+        const commandNames = commandFiles.map(file => {
+            const command = require(`../commands/${file}`);
+            return command.data.name;
+        });
+
         const version = packageJson.version;
         const uptime = process.uptime();
-        const uptimeFormatted = new Date(uptime * 1000).toISOString().substr(11, 8);
-
+        const uptimeFormatted = formatUptime(uptime);
         const latency = Date.now() - interaction.createdTimestamp;
 
         let embed = new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Help')
-            .setDescription(`Available commands:\n\`/create\`, \`/add\`, \`/gen\`, \`/restock\`, \`/help\`, \`/stock\``)
-            .addField('Bot Statistics', `Latency: ${latency}ms\nRunning on Soracx v${version}\nBot uptime: ${uptimeFormatted}`)
-            .setFooter('Coded by [3z4k](https://github.com/3z4k/Soracx-GenBot)');
+            .setTitle('Help and Bot Statistics')
+            .setDescription('Available commands:\n`' + commandNames.join('`, `') + '`')
+            .addFields(
+                { name: ':gear: Commands', value: client.commands.size.toString(), inline: false },
+                { name: ':file_folder: Soracx Version', value: version, inline: false },
+                { name: ':timer: Uptime', value: uptimeFormatted, inline: false },
+                { name: ':stopwatch: Ping', value: `${latency}ms`, inline: false },
+                { name: ':man_technologist: Developer', value: '[3z4k](https://github.com/3z4k)', inline: false }
+            )
+            .setFooter('Credits to Soracx');
 
         await interaction.reply({ embeds: [embed] });
     },
 };
+
+function formatUptime(seconds) {
+    const days = Math.floor(seconds / (3600 * 24));
+    const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${days} Days︲${hours} Hrs︲${minutes} Mins︲${secs} Secs Uptime`;
+}
+
