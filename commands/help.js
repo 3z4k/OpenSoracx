b0cf895fee413ac2db82a898cde7fbf147344133
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-07-10 09:08:10 +0545

    Created help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Displays help information.'),
+    async execute(interaction) {
+        let embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Help')
+            .setDescription('Available commands:\n`/create`, `/add`, `/gen`, `/restock`, `/help`, `/stock`')
+            .setFooter('Credits to Soracx');
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
