164bfe113c9a6743f73fd2ea73d69d2215b20f06
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-07-10 16:01:29 +0545

    Old help.js

--- commands/help.js
+++ commands/help.js
@@ -1,48 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-const packageJson = require('../package.json'); 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Displays help information and bot statistics.'),
+        .setDescription('Displays help information.'),
     async execute(interaction) {
-        const client = interaction.client;
-        const commandFiles = fs.readdirSync(path.resolve(__dirname, '../commands')).filter(file => file.endsWith('.js'));
-        const commandNames = commandFiles.map(file => {
-            const command = require(`../commands/${file}`);
-            return command.data.name;
-        });
-
-        const version = packageJson.version;
-        const uptime = process.uptime();
-        const uptimeFormatted = formatUptime(uptime);
-        const latency = Date.now() - interaction.createdTimestamp;
-
         let embed = new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Help and Bot Statistics')
-            .setDescription('Available commands:\n`' + commandNames.join('`, `') + '`')
-            .addFields(
-                { name: ':gear: Commands', value: client.commands.size.toString(), inline: false },
-                { name: ':file_folder: Soracx Version', value: version, inline: false },
-                { name: ':timer: Uptime', value: uptimeFormatted, inline: false },
-                { name: ':stopwatch: Ping', value: `${latency}ms`, inline: false },
-                { name: ':man_technologist: Developer', value: '[3z4k](https://github.com/3z4k)', inline: false }
-            )
+            .setTitle('Help')
+            .setDescription('Available commands:\n/create, /add, /gen, /restock, /help, /stock')
             .setFooter('Credits to Soracx');
 
         await interaction.reply({ embeds: [embed] });
     },
 };
-
-function formatUptime(seconds) {
-    const days = Math.floor(seconds / (3600 * 24));
-    const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${days} Days︲${hours} Hrs︲${minutes} Mins︲${secs} Secs Uptime`;
-}
 
