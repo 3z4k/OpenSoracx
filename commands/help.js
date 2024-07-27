91be1241128ab66c508e4f914a862001725c4e02
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-07-27 13:46:30 +0545

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,34 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8'));
+const version = packageJson.version;
+const author = packageJson.author;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Displays help information.'),
     async execute(interaction) {
+        const ping = Math.round(interaction.client.ws.ping);
+        const clientUsername = interaction.client.user.username;
+
         let embed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Help')
-            .setDescription('Available commands:\n/create, /add, /gen, /restock, /help, /stock')
-            .setFooter('Credits to Soracx');
+            .setColor('#2B2D31')
+            .setTitle(`Information about ${clientUsername}`)
+            .setDescription('An advanced service bot for managing and generating accounts.')
+            .addFields(
+                { name: '__My Features__', value: '* Manage and generate accounts.\n* Notify server restocks.\n* Display available stocks.\n* View bot statistics and more!', inline: false },
+                { name: '__How do you use me?__', value: 'Use the following commands to interact with me:\n\n`/create` - Create a new service.\n`/add` - Add an account to a service.\n`/gen` - Generate an account from a service and send it to DMs.\n`/restock` - Notify the server of a service restock.\n`/stock` - Display available stocks.\n`/help` - Display this help menu.', inline: false },
+                { name: '__STATS:__', value: `* Developer: ${author}\n* Ping: ${ping}ms\n* Version: ${version}\n* [Source Code](https://github.com/3z4k/Soracx-GenBot)`, inline: false },
+            )
+            .setFooter('Credits to Soracx')
+            .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
     },
 };
+
 
