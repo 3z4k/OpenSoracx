f0222780043c8d2e8220d0b9ca5ed0b83a3174ea
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-07-31 10:28:16 +0545

    Update help.js
    Added Support server link

--- commands/help.js
+++ commands/help.js
@@ -22,7 +22,7 @@ module.exports = {
             .addFields(
                 { name: '__My Features__', value: '* Manage and generate accounts.\n* Notify server restocks.\n* Display available stocks.\n* View bot statistics and more!', inline: false },
                 { name: '__How do you use me?__', value: 'Use the following commands to interact with me:\n\n`/create` - Create a new service.\n`/add` - Add an account to a service.\n`/gen` - Generate an account from a service and send it to DMs.\n`/restock` - Notify the server of a service restock.\n`/stock` - Display available stocks.\n`/help` - Display this help menu.', inline: false },
-                { name: '__STATS:__', value: `* Developer: ${author}\n* Ping: ${ping}ms\n* Version: ${version}\n* [Source Code](https://github.com/3z4k/Soracx-GenBot)`, inline: false },
+                { name: '__STATS:__', value: `* Developer: ${author}\n* Ping: ${ping}ms\n* Version: ${version}\n* [SourceCode](https://github.com/3z4k/Soracx-GenBot) • [Support Server](https://discord.gg/bmfUv56e)`, inline: false },
             )
             .setFooter('Credits to Soracx')
             .setTimestamp();
