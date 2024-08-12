e37aa6202d54509f50a4bd9cbbadc9abe8e0c524
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-08-12 09:03:04 +0545

    Update create.js

--- commands/create.js
+++ commands/create.js
@@ -7,28 +7,43 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('create')
         .setDescription('Creates a txt file for the service.')
-        .addStringOption(option => 
+        .addStringOption(option =>
             option.setName('service')
                 .setDescription('The name of the service')
-                .setRequired(true)),
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('servicetype')
+                .setDescription('The type of service (free or premium)')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Free', value: 'Free' },
+                    { name: 'Premium', value: 'Premium' }
+                )),
     adminOnly: true,
     async execute(interaction) {
         const service = interaction.options.getString('service');
-        const filePath = path.join(__dirname, `../data/${service}.txt`);
+        const serviceType = interaction.options.getString('servicetype');
+        const folderPath = path.join(__dirname, `../data/${serviceType}Gen`);
+        const filePath = path.join(folderPath, `${service}.txt`);
 
         let embed = new MessageEmbed()
             .setColor('#0099ff')
             .setFooter('Credits to Soracx');
 
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+        }
+
         if (fs.existsSync(filePath)) {
             embed.setTitle('Service Exists')
-                .setDescription(`The service **${service}** already exists.`);
+                .setDescription(`The service **${service}** already exists in the ${serviceType} category.`);
         } else {
             fs.writeFileSync(filePath, '', 'utf8');
             embed.setTitle('Service Created')
-                .setDescription(`Service **${service}** has been created.`);
+                .setDescription(`Service **${service}** has been created in the ${serviceType} category.`);
         }
 
         await interaction.reply({ embeds: [embed] });
     },
 };
+
