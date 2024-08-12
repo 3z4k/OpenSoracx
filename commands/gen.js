db9ec28c7c7257a0f2edc4fce3759afa53330126
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-08-12 09:03:36 +0545

    Update gen.js

--- commands/gen.js
+++ commands/gen.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { genChannelId } = require('../config.json');
+const { genChannelId, freegenroleid, premiumgenroleid, premiumGenChannelId } = require('../config.json');
 
-function getServiceChoices() {
-    const dataPath = path.join(__dirname, '../data');
+const cooldowns = new Collection();
+
+function getServiceChoices(dir) {
+    const dataPath = path.join(__dirname, `../data/${dir}`);
     const files = fs.readdirSync(dataPath).filter(file => file.endsWith('.txt'));
     return files.map(file => ({ name: file.replace('.txt', ''), value: file.replace('.txt', '') }));
 }
@@ -14,20 +16,83 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('gen')
         .setDescription('Generates the first account of the service and sends it to DMs.')
-        .addStringOption(option => 
-            option.setName('service')
-                .setDescription('The name of the service')
-                .setRequired(true)
-                .addChoices(...getServiceChoices())
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('free')
+                .setDescription('Generate account from free services.')
+                .addStringOption(option => {
+                    const choices = getServiceChoices('FreeGen');
+                    choices.forEach(choice => {
+                        option.addChoices(choice);
+                    });
+                    return option
+                        .setName('service')
+                        .setDescription('The name of the free service')
+                        .setRequired(true);
+                })
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('premium')
+                .setDescription('Generate account from premium services.')
+                .addStringOption(option => {
+                    const choices = getServiceChoices('PremiumGen');
+                    choices.forEach(choice => {
+                        option.addChoices(choice);
+                    });
+                    return option
+                        .setName('service')
+                        .setDescription('The name of the premium service')
+                        .setRequired(true);
+                })
         ),
     async execute(interaction) {
-        const serviceChoices = getServiceChoices();
+        const subcommand = interaction.options.getSubcommand();
         const service = interaction.options.getString('service');
-        const filePath = path.join(__dirname, `../data/${service}.txt`);
+        const filePath = path.join(__dirname, `../data/${subcommand === 'free' ? 'FreeGen' : 'PremiumGen'}/${service}.txt`);
 
-        if (interaction.channelId !== genChannelId) {
+        if (subcommand === 'free' && interaction.channelId !== genChannelId) {
             return interaction.reply({
                 content: `This command can only be used in the <#${genChannelId}> channel.`,
+                ephemeral: true
+            });
+        }
+
+        if (subcommand === 'premium' && interaction.channelId !== premiumGenChannelId) {
+            return interaction.reply({
+                content: `This command can only be used in the <#${premiumGenChannelId}> channel.`,
+                ephemeral: true
+            });
+        }
+
+        if (subcommand === 'free' && interaction.member.roles.cache.has(freegenroleid)) {
+            const now = Date.now();
+            const timestamps = cooldowns.get(interaction.user.id);
+            const cooldownAmount = 20 * 60 * 1000; // 20 minutes
+
+            if (timestamps) {
+                const expirationTime = timestamps + cooldownAmount;
+                if (now < expirationTime) {
+                    const timeLeft = expirationTime - now;
+                    const minutes = Math.floor(timeLeft / (1000 * 60));
+                    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+                    const embed = new MessageEmbed()
+                        .setColor('#2B2D31')
+                        .setTitle('Cooldown Active')
+                        .setDescription(`You can use this command again in ${minutes}m ${seconds}s.`)
+                        .setFooter('Credits to Soracx');
+
+                    return interaction.reply({ embeds: [embed], ephemeral: true });
+                }
+            }
+            cooldowns.set(interaction.user.id, now);
+            setTimeout(() => cooldowns.delete(interaction.user.id), cooldownAmount);
+        }
+
+        if (subcommand === 'premium' && !interaction.member.roles.cache.has(premiumgenroleid)) {
+            return interaction.reply({
+                content: 'You do not have permission to use this command.',
                 ephemeral: true
             });
         }
@@ -75,3 +140,5 @@ module.exports = {
 
         await interaction.reply({ embeds: [embed], ephemeral: false });
     },
+};
+
