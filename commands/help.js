3ac284183a0b2ea00fc521384da4bed5caa74528
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-08-12 09:02:09 +0545

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,16 +13,15 @@ module.exports = {
         .setDescription('Displays help information.'),
     async execute(interaction) {
         const ping = Math.round(interaction.client.ws.ping);
-        const clientUsername = interaction.client.user.username;
 
         let embed = new MessageEmbed()
             .setColor('#2B2D31')
-            .setTitle(`Information about ${clientUsername}`)
+            .setTitle(`Information about ${interaction.client.user.username}`)
             .setDescription('An advanced service bot for managing and generating accounts.')
             .addFields(
                 { name: '__My Features__', value: '* Manage and generate accounts.\n* Notify server restocks.\n* Display available stocks.\n* View bot statistics and more!', inline: false },
-                { name: '__How do you use me?__', value: 'Use the following commands to interact with me:\n\n`/create` - Create a new service.\n`/add` - Add an account to a service.\n`/gen` - Generate an account from a service and send it to DMs.\n`/restock` - Notify the server of a service restock.\n`/stock` - Display available stocks.\n`/help` - Display this help menu.', inline: false },
-                { name: '__STATS:__', value: `* Developer: ${author}\n* Ping: ${ping}ms\n* Version: ${version}\n* [SourceCode](https://github.com/3z4k/Soracx-GenBot) • [Support Server](https://discord.gg/bmfUv56e)`, inline: false },
+                { name: '__How do you use me?__', value: 'Use the following commands to interact with me:\n\n`/create` - Create a new service.\n`/add` - Add an account to a service.\n`/gen` - Generate an account from a service and send it to DMs.\n`/freegen` - Generate a free account from a service and send it to DMs.\n`/premiumgen` - Generate a premium account from a service and send it to DMs.\n`/restock` - Notify the server of a service restock.\n`/stock` - Display available stocks.\n`/help` - Display this help menu.', inline: false },
+                { name: '__STATS:__', value: `* Developer: ${author}\n* Ping: ${ping}ms\n* Version: ${version}\n* [SourceCode](https://github.com/3z4k/Soracx-GenBot)`, inline: false },
             )
             .setFooter('Credits to Soracx')
             .setTimestamp();
@@ -30,5 +29,4 @@ module.exports = {
         await interaction.reply({ embeds: [embed] });
     },
 };
-
 
