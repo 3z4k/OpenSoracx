490b2f039a9b627550fd4c63f53ce22edd7a90e6
k4z3 <91467994+3z4k@users.noreply.github.com>
2024-11-30 06:51:01 -0800

    Update stock.js
    Better Stock Command

--- commands/stock.js
+++ commands/stock.js
@@ -2,50 +2,48 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const AsciiTable = require('ascii-table');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('stock')
-        .setDescription('Displays available stocks.'),
-    async execute(interaction) {
-        const freePath = path.join(__dirname, '../data/FreeGen');
-        const premiumPath = path.join(__dirname, '../data/PremiumGen');
+   data: new SlashCommandBuilder()
+       .setName('stock')
+       .setDescription('Displays comprehensive stock availability'),
+   async execute(interaction) {
+       const freePath = path.join(__dirname, '../data/FreeGen');
+       const premiumPath = path.join(__dirname, '../data/PremiumGen');
+       const freeFiles = fs.readdirSync(freePath).filter(file => file.endsWith('.txt'));
+       const premiumFiles = fs.readdirSync(premiumPath).filter(file => file.endsWith('.txt'));
 
-        const freeFiles = fs.readdirSync(freePath).filter(file => file.endsWith('.txt'));
-        const premiumFiles = fs.readdirSync(premiumPath).filter(file => file.endsWith('.txt'));
+       let freeStock = '';
+       let premiumStock = '';
 
-        let freeTable = new AsciiTable('Free Stock');
-        freeTable.setHeading('Service', 'Availability');
+       if (freeFiles.length > 0) {
+           freeFiles.forEach(file => {
+               const service = file.replace('.txt', '');
+               const accounts = fs.readFileSync(path.join(freePath, file), 'utf8').split('\n').filter(Boolean).length;
+               freeStock += `${service}: ${accounts} accounts\n`;
+           });
+       } else {
+           freeStock = 'No free services available at this time.';
+       }
 
-        if (freeFiles.length > 0) {
-            freeFiles.forEach(file => {
-                const service = file.replace('.txt', '');
-                const accounts = fs.readFileSync(path.join(freePath, file), 'utf8').split('\n').filter(Boolean).length;
-                freeTable.addRow(service, `${accounts} accounts`);
-            });
-        } else {
-            freeTable.addRow('No services available.', '');
-        }
+       if (premiumFiles.length > 0) {
+           premiumFiles.forEach(file => {
+               const service = file.replace('.txt', '');
+               const accounts = fs.readFileSync(path.join(premiumPath, file), 'utf8').split('\n').filter(Boolean).length;
+               premiumStock += `${service}: ${accounts} accounts\n`;
+           });
+       } else {
+           premiumStock = 'No premium services available at this time.';
+       }
 
-        let premiumTable = new AsciiTable('Premium Stock');
-        premiumTable.setHeading('Service', 'Availability');
-
-        if (premiumFiles.length > 0) {
-            premiumFiles.forEach(file => {
-                const service = file.replace('.txt', '');
-                const accounts = fs.readFileSync(path.join(premiumPath, file), 'utf8').split('\n').filter(Boolean).length;
-                premiumTable.addRow(service, `${accounts} accounts`);
-            });
-        } else {
-            premiumTable.addRow('No services available.', '');
-        }
-
-             const embed = new MessageEmbed()
-            .setColor('#2B2D31')
-            .setDescription('```\n' + freeTable.toString() + '\n```\n```\n' + premiumTable.toString() + '\n```')
-            .setFooter('Credits to Soracx');
-        await interaction.reply({ embeds: [embed] });
-    },
+       const stockOverview = new MessageEmbed()
+           .setColor('#2B2D31')
+           .setTitle('Stock Availability')
+           .setDescription('Free and premium stock levels for our services.')
+           .addField('**__Free Stock__**', '```\n' + freeStock + '```', true)
+           .addField('**__Premium Stock__**', '```\n' + premiumStock + '```', true)
+           .setFooter('Credits to Soracx');
+       
+       await interaction.reply({ embeds: [stockOverview] });
+   },
 };
-
